82fb655da543b47c2acab8dcf89d93c394948df5
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-14 18:42:54 -0700

    fix path renames

---
+++ server/routes/joke.routes.js
@@ -0,0 +1,13 @@
+const JokeController = require('../controllers/joke.controller');
+
+module.exports = (app) => {
+	// Read (All)
+	app.get('/api/jokes/', JokeController.findAllJokes);
+	// Read (One)
+	app.get('/api/jokes/:id', JokeController.findOneSingleJoke);
+	// Read (Random)
+	app.get('/api/jokes/random', JokeController.findRandomJoke);
+	app.put('/api/jokes/update/:id', JokeController.updateExistingJoke);
+	app.post('/api/jokes/new', JokeController.createNewJoke);
+	app.delete('/api/jokes/delete/:id', JokeController.deleteAnExistingJoke);
+};
