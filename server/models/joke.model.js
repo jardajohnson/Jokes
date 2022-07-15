10369f91a2f25ad6361892b93b2431daab642d27
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-14 18:27:50 -0700

    add joke schema

---
+++ server/models/joke.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema({
+	setup: {
+		type: String,
+		minlength: [10, "Setup must be at least 10 characters long."]
+	},
+
+	punchline: {
+		type: String,
+		minlength: [3, "Punch line must be at least 3 characters long."]
+	}
+});
+
+const Joke = mongoose.model("joke", JokeSchema);
+
+module.exports = Joke;
